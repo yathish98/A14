1bfb0e362b69809db41218117d88521f2f6e088c
yathish98 <36331974+yathish98@users.noreply.github.com>
2018-04-04 16:09:59 +0530

    Add files via upload

---
+++ OS assignment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<sys/types.h>
+#include<pthread.h>
+void main()
+{
+	int n,m;
+	printf("Enter the number of students in the queue: ");
+	scanf("%d",&n);
+	printf("Enter the number of faculty members in the queue: ");
+	scanf("%d",&m);
+	
+}
