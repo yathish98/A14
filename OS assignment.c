96daadaff092844e7c4b1339542aa59b5effafb3
yathish98 <36331974+yathish98@users.noreply.github.com>
2018-04-09 20:02:46 +0530

    Update OS assignment.c

--- OS assignment.c
+++ OS assignment.c
@@ -2,52 +2,55 @@
  
 int main() 
 { 
- 
-  int count,j,n,time,remain,flag=0,time_quantum; 
-  int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10]; 
-  printf("Enter Total Process:\t "); 
+  int student[1000],faculty[1000]; 
+  int count,j,n,time,remain,flag=0,tq,wait=0,turnaround=0,arr[10],burst[10],rem[10];
+  float avgwait,avgturn; 
+  printf("Enter number of processes: "); 
   scanf("%d",&n); 
   remain=n; 
   for(count=0;count<n;count++) 
   { 
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
-    scanf("%d",&at[count]); 
-    scanf("%d",&bt[count]); 
-    rt[count]=bt[count]; 
+  	printf("\nFor process P%d",count+1);
+    printf("\nEnter Arrival Time:"); 
+    scanf("%d",&arr[count]); 
+    printf("\nEnter burst time:");
+    scanf("%d",&burst[count]); 
+    rem[count]=burst[count]; 
   } 
-  printf("Enter Time Quantum:\t"); 
-  scanf("%d",&time_quantum); 
+  printf("Enter Time Quantum:"); 
+  scanf("%d",&tq); 
   printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
   for(time=0,count=0;remain!=0;) 
   { 
-    if(rt[count]<=time_quantum && rt[count]>0) 
+    if(rem[count]<=tq && rem[count]>0) 
     { 
-      time+=rt[count]; 
-      rt[count]=0; 
+      time+=rem[count]; 
+      rem[count]=0; 
       flag=1; 
     } 
-    else if(rt[count]>0) 
+    else if(rem[count]>0) 
     { 
-      rt[count]-=time_quantum; 
-      time+=time_quantum; 
+      rem[count]-=tq; 
+      time+=tq; 
     } 
-    if(rt[count]==0 && flag==1) 
+    if(rem[count]==0 && flag==1) 
     { 
       remain--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
-      wait_time+=time-at[count]-bt[count]; 
-      turnaround_time+=time-at[count]; 
+      printf("P%d\t|\t%d\t|\t%d\n",count+1,time-arr[count],time-arr[count]-burst[count]); 
+      wait+=time-arr[count]-burst[count]; 
+      turnaround+=time-arr[count]; 
       flag=0; 
     } 
     if(count==n-1) 
       count=0; 
-    else if(at[count+1]<=time) 
+    else if(arr[count+1]<=time) 
       count++; 
     else 
       count=0; 
   } 
-  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
-  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
-  
+  avgwait=wait*1.0/n;
+  avgturn=turnaround*1.0/n;
+  printf("\nAverage Waiting Time= %f\n",avgwait); 
+  printf("Avg Turnaround Time = %f",avgturn); 
   return 0; 
 }
